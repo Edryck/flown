d093ebf1b9de61b134f6c1f30a9b26a63f5d49cd
Edryck Nascimento <edryckfreitas@gmail.com>
2026-07-19 23:34:08 -0300

    feat(routes): add task routes

---
+++ backend/src/routes/task.routes.ts
@@ -0,0 +1,17 @@
+import type { FastifyInstance } from "fastify";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import * as taskController from "../controllers/task.controller.js";
+
+export async function taskRoutes(app: FastifyInstance) {
+  app.addHook("preHandler", authMiddleware);
+  app.get("/tasks", taskController.list);
+  app.post("/tasks", taskController.create);
+  app.patch("/tasks/reorder", taskController.reorder);
+  app.get("/tasks/:id", taskController.getById);
+  app.patch("/tasks/:id", taskController.update);
+  app.delete("/tasks/:id", taskController.softDelete);
+  app.post("/tasks/:id/restore", taskController.restore);
+  app.delete("/tasks/:id/permanent", taskController.permanentDelete);
+  app.get("/tasks/:id/subtasks", taskController.listSubtasks);
+  app.post("/tasks/:id/subtasks", taskController.createSubtask);
+}
